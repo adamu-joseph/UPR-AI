1b40e46f3774e200370c54b929fdbbf922498bd6
Adamu Joseph Ohigwere <ohigwerejosephadamu@gmail.com>
2026-06-12 17:06:42 +0100

    Corrected package installation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="UPR-AI",
+    name="upr_ai",
     version="1.0.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
